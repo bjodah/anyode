fc3851402970ef3f8cb6288a313dba195aa20c3c
Björn Dahlgren <bjodah@gmail.com>
2017-04-14 14:41:44 +0200

    More templates

--- include/anyode/anyode_blas_lapack.hpp
+++ include/anyode/anyode_blas_lapack.hpp
@@ -1,11 +1,12 @@
-#ifndef ANYODE_BLAS_LAPACK_HPP3FDF14C295FA11E6A327373BE69EEE93
-#define ANYODE_BLAS_LAPACK_HPP3FDF14C295FA11E6A327373BE69EEE93
-
+#pragma once
 extern "C" void dgemv_(const char* trans, int* m, int* n, const double* alpha, const double* a, int* lda,
-                       const double* x, int* incx, const double* beta, double* y, int* incy, int stupid_sundials=0);
+                       const double* x, int* incx, const double* beta, double* y, int* incy, int sundials__=0);
+extern "C" void sgemv_(const char* trans, int* m, int* n, const float* alpha, const float* a, int* lda,
+                       const float* x, int* incx, const float* beta, float* y, int* incy, int sundials__=0);
 
 extern "C" void dgesvd_(const char* jobu, const char* jobvt, int* m, int* n, const double* a,
-                       int* lda, double* s, double* u, int* ldu, double* vt, int* ldvt,
-                       double* work, int* lwork, int* info );
-
-#endif /* ANYODE_BLAS_LAPACK_HPP3FDF14C295FA11E6A327373BE69EEE93 */
+                        int* lda, double* s, double* u, int* ldu, double* vt, int* ldvt,
+                        double* work, int* lwork, int* info );
+extern "C" void sgesvd_(const char* jobu, const char* jobvt, int* m, int* n, const float* a,
+                        int* lda, float* s, float* u, int* ldu, float* vt, int* ldvt,
+                        float* work, int* lwork, int* info );
--- include/anyode/anyode_matrix.hpp
+++ include/anyode/anyode_matrix.hpp
@@ -3,40 +3,42 @@
 
 namespace AnyODE {
 
+    template<typename Real_t>
     struct MatrixView {
-        double * m_data;
+        Real_t * m_data;
         int m_nr, m_nc, m_ld;
         bool m_own_data;
-        MatrixView (double * const data, int nr, int nc, int ld, bool own_data) :
+        MatrixView (Real_t * const data, int nr, int nc, int ld, bool own_data) :
             m_data(data), m_nr(nr), m_nc(nc), m_ld(ld), m_own_data(own_data) {}
         virtual ~MatrixView(){
             if (m_own_data and m_data)
                 delete []m_data;
         }
-        virtual double& operator()(int ri, int ci) = 0;
-        virtual void dot_vec(const double * const, double * const) = 0;
-        virtual void set_to_eye_plus_scaled_mtx(double, MatrixView&) = 0;
+        virtual Real_t& operator()(int ri, int ci) = 0;
+        virtual void dot_vec(const Real_t * const, Real_t * const) = 0;
+        virtual void set_to_eye_plus_scaled_mtx(Real_t, MatrixView&) = 0;
     };
 
-    struct DenseMatrixView : MatrixView {
+    template<typename Real_t>
+    struct DenseMatrixView : MatrixView<Real_t> {
         bool m_colmaj;
-        DenseMatrixView(double * const data, int nr, int nc, int ld, bool colmaj) :
-         MatrixView(data ? data : new double[ld*(colmaj ? nc : nr)], nr, nc, ld, data == nullptr),
+        DenseMatrixView(Real_t * const data, int nr, int nc, int ld, bool colmaj) :
+         MatrixView(data ? data : new Real_t[ld*(colmaj ? nc : nr)], nr, nc, ld, data == nullptr),
          m_colmaj(colmaj) {}
-        virtual double& operator()(int ri, int ci) override {
+        virtual Real_t& operator()(int ri, int ci) override {
             const int imaj = m_colmaj ? ci : ri;
             const int imin = m_colmaj ? ri : ci;
             return m_data[imaj*m_ld + imin];
         }
-        virtual void dot_vec(const double * const vec, double * const out) override {
-            double alpha=1, beta=0;
+        virtual void dot_vec(const Real_t * const vec, Real_t * const out) override {
+            Real_t alpha=1, beta=0;
             int inc=1;
             char trans= m_colmaj ? 'N' : 'T';
             int sundials_dummy = 0;
-            dgemv_(&trans, &(m_nr), &(m_nc), &alpha, m_data, &m_ld, const_cast<double *>(vec),
+            dgemv_(&trans, &(m_nr), &(m_nc), &alpha, m_data, &m_ld, const_cast<Real_t *>(vec),
                    &inc, &beta, out, &inc, sundials_dummy);
         }
-        virtual void set_to_eye_plus_scaled_mtx(double scale, MatrixView& other) override {
+        virtual void set_to_eye_plus_scaled_mtx(Real_t scale, MatrixView& other) override {
             for (int imaj = 0; imaj < (m_colmaj ? m_nc : m_nr); ++imaj){
                 for (int imin = 0; imin < (m_colmaj ? m_nr : m_nc); ++imin){
                     const int ri = m_colmaj ? imin : imaj;
