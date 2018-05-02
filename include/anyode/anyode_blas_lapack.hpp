eb53062fd97bfd917d418f1e50ce682747b0570c
Björn Dahlgren <bjodah@gmail.com>
2018-05-02 13:57:22 +0200

    remove constexpr

--- include/anyode/anyode_blas_lapack.hpp
+++ include/anyode/anyode_blas_lapack.hpp
@@ -37,7 +37,7 @@ extern "C" void sgbtrs_(const char* trans, const int* n, const int* kl, const in
 #define PROXY_SPECIALIZATION(CLS_NAME, TYPE, CALLBACK_NAME)        \
     template<> struct CLS_NAME ## _callback<TYPE> {                \
         template<class...Args>                                     \
-        constexpr void operator()(Args&&... args) const noexcept { \
+        void operator()(Args&&... args) const noexcept {           \
             CALLBACK_NAME(std::forward<Args>(args)...);            \
         }                                                          \
     };
