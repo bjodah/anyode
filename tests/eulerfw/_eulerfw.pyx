815fc0eae0dabc812116f4384295eb7de00a5486
Björn Ingvar Dahlgren <bjodah@gmail.com>
2021-07-15 12:45:23 +0200

    c++17

--- tests/eulerfw/_eulerfw.pyx
+++ tests/eulerfw/_eulerfw.pyx
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; mode: cython -*-
 # cython: language_level = 3str
 # distutils: language = c++
-# distutils: extra_compile_args = -std=c++11
+# distutils: extra_compile_args = -std=c++17
 
 from cpython.ref cimport PyObject
 cimport numpy as cnp
