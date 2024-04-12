cad4fcc6ce0a528b7e5549571f18cfa24893d3b6
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-12 14:39:24 +0200

    tweak test script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,7 +4,7 @@ export PATH=:/opt-2/gcc-13/bin:$PATH
 
 for py_vers in '3.11-apt-deb' '3.11.*-debug' '3.12.*-debug' '3.12.*-asan' '3.12.*-release' '3.11.*-asan'; do
     for dir in midpoint eulerfw; do
-        cd tests/midpoint
+        cd tests/$dir
         if [ -d ./build/ ]; then
             rm -r ./build/
         fi
@@ -14,10 +14,12 @@ for py_vers in '3.11-apt-deb' '3.11.*-debug' '3.12.*-debug' '3.12.*-asan' '3.12.
 done
 
 for dir in tests/decomp tests/matrix; do 
-    cd $dir; make -B DEFINES="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2"; cd -
-    cd $dir; make -B DEFINES=-DNDEBUG; cd -
+    cd $dir
+    make -B DEFINES="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2"
+    make -B DEFINES=-DNDEBUG
     LIBCXX_ROOT=$(compgen -G "/opt-2/libcxx??-debug")
-    cd $dir; make -B CXX=clang++ EXTRA_FLAGS="-fsanitize=address -nostdinc++ -isystem ${LIBCXX_ROOT}/include/c++/v1" LDFLAGS="-nostdlib++ -Wl,-rpath,${LIBCXX_ROOT}/lib -L${LIBCXX_ROOT}/lib" LDLIBS="-lc++"; cd -
+    make -B CXX=clang++ EXTRA_FLAGS="-fsanitize=address -nostdinc++ -isystem ${LIBCXX_ROOT}/include/c++/v1" LDFLAGS="-nostdlib++ -Wl,-rpath,${LIBCXX_ROOT}/lib -L${LIBCXX_ROOT}/lib" LDLIBS="-lc++"
     LIBCXX_ROOT=$(compgen -G "/opt-2/libcxx??-asan")
-    cd $dir; make -B CXX=clang++ EXTRA_FLAGS="-fsanitize=address -nostdinc++ -isystem ${LIBCXX_ROOT}/include/c++/v1" LDFLAGS="-nostdlib++ -Wl,-rpath,${LIBCXX_ROOT}/lib -L${LIBCXX_ROOT}/lib" LDLIBS="-lc++" DEFINES=-DNDEBUG; cd -
+    make -B CXX=clang++ EXTRA_FLAGS="-fsanitize=address -nostdinc++ -isystem ${LIBCXX_ROOT}/include/c++/v1" LDFLAGS="-nostdlib++ -Wl,-rpath,${LIBCXX_ROOT}/lib -L${LIBCXX_ROOT}/lib" LDLIBS="-lc++" DEFINES=-DNDEBUG
+    cd -
 done
