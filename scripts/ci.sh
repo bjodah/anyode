003caf3b16a67a1c085ce93448313f6f85b20ce8
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-12 15:06:49 +0200

    tweak test matrix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,16 +2,6 @@
 
 export PATH=:/opt-2/gcc-13/bin:$PATH
 
-for py_vers in '3.11-apt-deb' '3.11.*-debug' '3.12.*-debug' '3.12.*-asan' '3.12.*-release' '3.11.*-asan'; do
-    for dir in midpoint eulerfw; do
-        cd tests/$dir
-        if [ -d ./build/ ]; then
-            rm -r ./build/
-        fi
-        ASAN_OPTIONS=detect_leaks=0 $(compgen -G "/opt-3/cpython-v${py_vers}/bin/python3") -m pytest -v
-        cd -
-    done
-done
 
 for dir in tests/decomp tests/matrix; do 
     cd $dir
@@ -22,4 +12,15 @@ for dir in tests/decomp tests/matrix; do
     LIBCXX_ROOT=$(compgen -G "/opt-2/libcxx??-asan")
     make -B CXX=clang++ EXTRA_FLAGS="-fsanitize=address -nostdinc++ -isystem ${LIBCXX_ROOT}/include/c++/v1" LDFLAGS="-nostdlib++ -Wl,-rpath,${LIBCXX_ROOT}/lib -L${LIBCXX_ROOT}/lib" LDLIBS="-lc++" DEFINES=-DNDEBUG
     cd -
+done
+
+for py_vers in '3.11-apt-deb' '3.11.*-debug' '3.12.*-debug' '3.12.*-asan' '3.12.*-release' '3.11.*-asan'; do
+    for dir in midpoint eulerfw; do
+        cd tests/$dir
+        if [ -d ./build/ ]; then
+            rm -r ./build/
+        fi
+        ASAN_OPTIONS=detect_leaks=0 $(compgen -G "/opt-3/cpython-v${py_vers}/bin/python3") -m pytest -v
+        cd -
+    done
 done
